6ffa634ce4b7b13fab459af882629fb596094235
tianshi <leowangx2013@outlook.com>
2021-01-04 23:47:02 -0600

    Finish checking

--- stfnet.py
+++ stfnet.py
@@ -4,171 +4,134 @@ import math
 import tensorflow as tf
 
 from stf_filter_layer import STFFilterLayer
+from stf_conv_layer import STFConvLayer
 
 class STFNet(tf.keras.Model):
-	def __init__(self, 
-				batch_size,
-				fft_list, 
-				kernel_size, 
-				class_num = 10,
-				sensor_axis=3,
-				c_out=64,
-				act_domain='time',
-				dropout_rate=0.2,
-				reuse=True,
-				name='STFNet'):
-		super(STFNet, self).__init__(name=name)
-		self.batch_size = batch_size
-		self.fft_list = fft_list
-		self.kernel_size = kernel_size
-		self.class_num = class_num
-		self.sensor_axis = 3
-		self.c_out = 64
-		self.act_domain = 'time'
-		self.dropout_rate = dropout_rate
-		self.reuse = True
+    def __init__(
+        self,
+        layer_type,
+        class_num,
+        sensor_number,
+        sensor_channel,
+        batch_size,
+        fft_list=[16, 32, 64, 128],
+        kernel_size=3,
+        c_out=64,
+        act_domain="time",
+        dropout_rate=0.2,
+        name="STFNet",
+    ):
+        super(STFNet, self).__init__(name=name)
 
-		self.acc_layer1 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.sensor_axis, 
-								self.c_out,
-								self.act_domain, 
-								self.reuse,
-								'acc_layer1'
-								)
-		self.acc_dropout1 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='acc_dropout1')
-		self.acc_layer2 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.c_out, 
-								self.c_out,
-								self.act_domain, 
-								self.reuse,
-								'acc_layer2'
-								)
-		self.acc_dropout2 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='acc_dropout2')
-		self.acc_layer3 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.c_out, 
-								self.c_out//2,
-								self.act_domain, 
-								self.reuse,
-								'acc_layer3'
-								)
-		self.acc_dropout3 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out//2],
-								name='acc_dropout3')
+        self.layer_type = layer_type
+        self.sensor_number = sensor_number
+        self.fft_list = fft_list
+        self.kernel_size = kernel_size
+        self.act_domain = "time"
+        self.dropout_rate = dropout_rate
 
-		self.gyr_layer1 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.sensor_axis, 
-								self.c_out,
-								self.act_domain, 
-								self.reuse,
-								'gyr_layer1'
-								)
-		self.gyr_dropout1 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='gyr_dropout1')
-		self.gyr_layer2 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.c_out, 
-								self.c_out,
-								self.act_domain, 
-								self.reuse,
-								'gyr_layer2'
-								)
-		self.gyr_dropout2 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='gyr_dropout2')
-		self.gyr_layer3 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.c_out, 
-								self.c_out//2,
-								self.act_domain, 
-								self.reuse,
-								'gyr_layer3'
-								)
-		self.gyr_dropout3 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out//2],
-								name='gyr_dropout3')
+        self.sensor_stf_layers = []
+        self.sensor_dropout_layers = []
 
-		self.fusion_layer1 = STFFilterLayer(self.fft_list, 
-								self.kernel_size,
-								self.c_out, 
-								self.c_out,
-								self.act_domain, 
-								self.reuse,
-								'fusion_layer1'
-								)
-		self.fusion_dropout1 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='gyr_dropout1')
-		self.fusion_layer2 = STFFilterLayer(self.fft_list, 
-						self.kernel_size,
-						self.c_out, 
-						self.c_out,
-						self.act_domain, 
-						self.reuse,
-						'fusion_layer2'
-						)
-		self.fusion_dropout2 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='gyr_dropout2')
-		self.fusion_layer3 = STFFilterLayer(self.fft_list, 
-						self.kernel_size,
-						self.c_out, 
-						self.c_out,
-						self.act_domain, 
-						self.reuse,
-						'fusion_layer3'
-						)
-		self.fusion_dropout3 = tf.keras.layers.Dropout(self.dropout_rate,
-								noise_shape=[self.batch_size, 1, self.c_out],
-								name='gyr_dropout3')
+        for s in range(sensor_number):
+            self.sensor_stf_layers.append(
+                self.__create_stf_layer(
+                    sensor_channel, c_out, "sensor{}_stf1".format(s)
+                )
+            )
+            self.sensor_dropout_layers.append(
+                self.__create_dropout_layer(
+                    [batch_size, 1, c_out], "sensor{}_dropout1".format(s)
+                )
+            )
 
-		self.final_dense = tf.keras.layers.Dense(self.class_num, name='final_dense')
+            self.sensor_stf_layers.append(
+                self.__create_stf_layer(c_out, c_out, "sensor{}_stf2".format(s))
+            )
+            self.sensor_dropout_layers.append(
+                self.__create_dropout_layer(
+                    [batch_size, 1, c_out], "sensor{}_dropout2".format(s)
+                )
+            )
 
-	def call(self, inputs, training=True):
-		acc_inputs, gyr_inputs = tf.split(inputs,
-									num_or_size_splits=2,
-									axis=2)
-		x_acc = self.acc_layer1(acc_inputs)
+            self.sensor_stf_layers.append(
+                self.__create_stf_layer(
+                    c_out, c_out // sensor_number, "sensor{}_stf3".format(s)
+                )
+            )
+            self.sensor_dropout_layers.append(
+                self.__create_dropout_layer(
+                    [batch_size, 1, c_out // sensor_number],
+                    "sensor{}_dropout3".format(s),
+                )
+            )
 
-		if training == True:
-			x_acc = self.acc_dropout1(x_acc)
-		x_acc = self.acc_layer2(x_acc)
-		if training == True:
-			x_acc = self.acc_dropout2(x_acc)
-		x_acc = self.acc_layer3(x_acc)
-		if training == True:
-			x_acc = self.acc_dropout3(x_acc)
+        self.fusion_layer1 = self.__create_stf_layer(
+            c_out // sensor_number * sensor_number, c_out, "fusion_stf1"
+        )
+        self.fusion_dropout1 = self.__create_dropout_layer(
+            [batch_size, 1, c_out], "fusion_dropout1"
+        )
 
-		x_gyr = self.gyr_layer1(gyr_inputs)
-		if training == True:
-			x_gyr = self.gyr_dropout1(x_gyr)
-		x_gyr = self.gyr_layer2(x_gyr)
-		if training == True:
-			x_gyr = self.gyr_dropout2(x_gyr)
-		x_gyr = self.gyr_layer3(x_gyr)
-		if training == True:
-			x_gyr = self.gyr_dropout3(x_gyr)
+        self.fusion_layer2 = self.__create_stf_layer(c_out, c_out, "fusion_stf2")
+        self.fusion_dropout2 = self.__create_dropout_layer(
+            [batch_size, 1, c_out], "fusion_dropout2"
+        )
 
-		x_fusion = tf.concat([x_acc, x_gyr], 2)
-		x_fusion = self.fusion_layer1(x_fusion)
-		if training == True:
-			x_fusion = self.fusion_dropout1(x_fusion)
-		x_fusion = self.fusion_layer2(x_fusion)
-		if training == True:
-			x_fusion = self.fusion_dropout2(x_fusion)
-		x_fusion = self.fusion_layer3(x_fusion)
-		if training == True:
-			x_fusion = self.fusion_dropout3(x_fusion)
+        self.fusion_layer3 = self.__create_stf_layer(c_out, c_out, "fusion_stf3")
+        self.fusion_dropout3 = self.__create_dropout_layer(
+            [batch_size, 1, c_out], "fusion_dropout3"
+        )
 
-		x_fusion = tf.reduce_mean(x_fusion, 1)
-		outputs = self.final_dense(x_fusion)
+        self.final_dense = tf.keras.layers.Dense(class_num, name="final_dense")
 
-		return outputs
+    def __create_stf_layer(self, c_in, c_out, name):
+        if self.layer_type == "filter":
+            layer = STFFilterLayer(
+                self.fft_list, self.kernel_size, c_in, c_out, self.act_domain, name
+            )
+        else:
+            layer = STFConvLayer(
+                self.fft_list, self.kernel_size, c_in, c_out, self.act_domain, name
+            )
+
+        return layer
+
+    def __create_dropout_layer(self, noise_shape, name):
+        layer = tf.keras.layers.Dropout(
+            self.dropout_rate, noise_shape=noise_shape, name=name
+        )
+        return layer
+
+    def call(self, inputs, training=True):
+        sensor_inputs = tf.split(inputs, num_or_size_splits=self.sensor_number, axis=2)
+
+        for n_sensor in range(self.sensor_number):
+            for n_layer in range(3):
+                sensor_inputs[n_sensor] = self.sensor_stf_layers[n_layer](
+                    sensor_inputs[n_sensor]
+                )
+                if training:
+                    sensor_inputs[n_sensor] = self.sensor_dropout_layers[n_layer](
+                        sensor_inputs[n_sensor]
+                    )
+
+        fusion_inputs = tf.concat(sensor_inputs, 2)
+
+        fusion_inputs = self.fusion_layer1(fusion_inputs)
+        if training:
+            fusion_inputs = self.fusion_dropout1(fusion_inputs)
+
+        fusion_inputs = self.fusion_layer2(fusion_inputs)
+        if training:
+            fusion_inputs = self.fusion_dropout2(fusion_inputs)
+
+        fusion_inputs = self.fusion_layer3(fusion_inputs)
+        if training:
+            fusion_inputs = self.fusion_dropout3(fusion_inputs)
+
+        fusion_inputs = tf.reduce_mean(fusion_inputs, 1)
+
+        outputs = self.final_dense(fusion_inputs)
+
+        return outputs
